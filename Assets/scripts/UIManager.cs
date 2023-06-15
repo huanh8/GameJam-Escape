8b1f7fd63585fec8f1c0f80d12236b3fb3a32dbc
Evan Huang <huanh8@student.op.ac.nz>
2023-06-15 15:55:20 +1200

    update Task UI

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -7,6 +7,9 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
     public TextMeshProUGUI keyText;
+    public TextMeshProUGUI taskText;
+    public TextMeshProUGUI abilityText;
+    public int keyLeft;
 
     private void Awake() {
         if(instance == null)
@@ -18,10 +21,23 @@ public class UIManager : MonoBehaviour
     }
 
     private void Update() {
+
         UpdateKeyText(GameManager.instance.keyCount);
+        keyLeft= GameManager.instance.maxKeyCount - GameManager.instance.keyCount;
+        UpdateTaskText();
     }
+
     public void UpdateKeyText(int keyCount)
     {
-        keyText.text = "x "+keyCount.ToString();
+        keyText.text = "x "+ keyCount.ToString();
+    }
+    public void UpdateTaskText()
+    {
+        if (keyLeft == 0)
+        {
+            taskText.text = $"Task : Find the door to escape.";
+            return;
+        }
+        taskText.text = $"Task : Find {keyLeft.ToString()} key(s) to unlock the door.";
     }
 }
